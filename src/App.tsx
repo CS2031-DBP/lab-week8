492745a988ff73a1b92a452f96dec0cfe6f8820c
mateonoel2 <mateo.noel@utec.edu.pe>
2024-10-09 08:12:29 -0500

    hola

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import StudentForm from "./studentForm";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/form" element={<StudentForm />} />
+        </Routes>
+        <div className="flex flex-col justify-center items-center h-screen">
+          <h1 className="text-2xl font-bold">
+            Welcome to the Student Registration App
+          </h1>
+          <a href="/form" className="text-blue-500 font-bold">
+            Register
+          </a>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
